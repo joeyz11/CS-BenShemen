3c623d9c5c2b348a4c2c017c34c648419c3cf555
Joey Zheng <joeyzheng2002@gmail.com>
2023-01-30 12:33:20 +0200

    snake game

--- Aragio_test/game.py
+++ Aragio_test/game.py
@@ -1,57 +1,79 @@
 import pygame as pg
 from random import randrange
 from sys import exit
+
+
+# global variables
 WINDOW = 1000
 TILE_SIZE = 50
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+
+
+# helper function
 def get_random_position(): return [randrange(*RANGE), randrange(*RANGE)]
 
 
+# snake data
 snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
-snake.center = get_random_position()
+snake.center = get_random_position()  # head of snake
 length = 1
 segments = [snake.copy()]
 snake_dir = (0, 0)
-time, time_step = 0, 110
-food = snake.copy()
-food.center = get_random_position()
-screen = pg.display.set_mode([WINDOW]*2)
-clock = pg.time.Clock()
 dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
 
+# food data
+food = snake.copy()
+food.center = get_random_position()
+
+# game set up
+time, time_step = 0, 110
+screen = pg.display.set_mode([WINDOW]*2)
+clock = pg.time.Clock()
+
+# game loop
 while True:
     clock.tick(60)
     screen.fill('black')
+
+    # draw food
+    pg.draw.rect(screen, 'red', food)
+
+    ################################
+    # TODO 1: Draw the snake! HINT: Look at how food is drawn and use a for loop to loop over `segments`
+    # Write in here
+
+    ################################
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w and dirs[pg.K_w]:
+            if event.key == pg.K_w and dirs[pg.K_w]:  # go up when w is pressed
                 snake_dir = (0, -TILE_SIZE)
                 dirs = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
-            if event.key == pg.K_s and dirs[pg.K_s]:
-                snake_dir = (0, TILE_SIZE)
-                dirs = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
-            if event.key == pg.K_a and dirs[pg.K_a]:
-                snake_dir = (-TILE_SIZE, 0)
-                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
-            if event.key == pg.K_d and dirs[pg.K_d]:
-                snake_dir = (TILE_SIZE, 0)
-                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
+            #################################
+            # TODO 2: implement going in the other 3 directions!
+            # Write in here
+
+            #################################
     # check borders and selfeating
     self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
-    if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
-        snake.center, food.center = get_random_position(), get_random_position()
-        length, snake_dir = 1, (0, 0)
+    # TODO 3: Implement top and bottom boundaries in the if statement below
+    if snake.left < 0 or snake.right > WINDOW or self_eating:
+        #################################
+        # TODO 4: What happens when the snake loses? The game starts over!
+        #         So re-initialize `snake.center`, `food.center`, `length`, `snake_dir`, and `dirs`
+        #         `segments` is already re-initialized for you!
+        # Write in here
         segments = [snake.copy()]
+
+        #################################
+
     # check food
     if snake.center == food.center:
         food.center = get_random_position()
         length += 1
-    # draw food
-    [pg.draw.rect(screen, 'red', food)]
-    # draw snake
-    [pg.draw.rect(screen, 'green', segment) for segment in segments]
+
     # move snake
     time_now = pg.time.get_ticks()
     if time_now - time > time_step:
@@ -59,5 +81,6 @@ while True:
         snake.move_ip(snake_dir)
         segments.append(snake.copy())
         segments = segments[-length:]
-        print(segments)
+
+    # update screen
     pg.display.update()
